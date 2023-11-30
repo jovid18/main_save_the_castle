4898d8a602ff9c618237086013969e3379974018
조성현 <cshcho99@gmail.com>
2023-12-01 06:48:29 +0900

    Apply teleport Modify Gamedirector script

--- Assets/Scripts/Teleports.cs
+++ Assets/Scripts/Teleports.cs
@@ -8,8 +8,11 @@ public class Teleports : MonoBehaviour
     public InputActionReference triggerAction; // 트리거 입력 액션에 대한 레퍼런스
     public Transform loc1;
     public Transform loc2;
+    public GameObject spot1;
+    public GameObject spot2;
     bool pos = false;
-    int checktime = 30;
+    float span = 1.0f;
+    float time = 0;
     private void Start()
     {
         Application.targetFrameRate = 60;
@@ -19,12 +22,9 @@ public class Teleports : MonoBehaviour
 
     private void Update()
     {
+        this.time += Time.deltaTime;
         // 트리거 입력 확인
-        if (checktime > 0)
-        {
-            checktime--;
-        }
-        else
+        if (this.time>this.span)
         {
             if (triggerAction.action.ReadValue<float>() > 0.5f)
             {
@@ -32,14 +32,18 @@ public class Teleports : MonoBehaviour
                 if (pos)
                 {
                     Tele1(); // 텔레포트 함수 호출
+                    spot1.SetActive(false); //spot1 숨기기
+                    spot2.SetActive(true); //spot2 보이기
                     pos = false;
                 }
                 else
                 {
                     Tele2(); // 텔레포트 함수 호출
+                    spot1.SetActive(true); //spot1 보이기
+                    spot2.SetActive(false); //spot2 숨기기
                     pos = true;
                 }
-                checktime = 30;
+                this.time = 0;
             }
 
         }
