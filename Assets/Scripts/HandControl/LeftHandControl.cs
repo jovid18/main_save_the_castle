472360fd2d6266d439d23549b66779112e5ddb9c
Dongho <pridesam@snu.ac.kr>
2023-12-01 17:58:46 +0900

    Add features on left hand controller
    1. Force to grab a bow.
    2. Do always grab and pinch animation to seem like always grab it.
    3. Deactivate all lefthand Trigger Input to prevent from releasing arrows.

---
+++ Assets/Scripts/HandControl/LeftHandControl.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// 1. Force to grab a bow.
+/// 2. Do always grab animate
+/// </summary>
+public class LeftHandControl : MonoBehaviour
+{
+    public InputActionProperty[] toDisableInputs;
+    public Animator handAnimator;
+    public XRDirectInteractor leftHandInteractor;  // 왼손 컨트롤러의 XR Direct Interactor
+    public XRGrabInteractable objectToGrab;       // 자동으로 그랩할 오브젝트
+
+    void Start()
+    {
+        if (leftHandInteractor == null || objectToGrab == null)
+        {
+            Debug.LogError("References not set in the AutoGrabObject script");
+            return;
+        }
+        
+        //// Call SelectEnter with the eventArgs
+        leftHandInteractor.interactionManager.SelectEnter(leftHandInteractor, objectToGrab);
+
+        // override onSelectExit event to force grab
+        objectToGrab.selectExited.AddListener(ForceMaintainGrab);
+    }
+
+    
+    void Update()
+    {
+        // Always animate by giving max float value to animator.
+        float value = 0.99f;
+        handAnimator.SetFloat("Trigger", value);
+        handAnimator.SetFloat("Grip", value);
+
+
+        // debug
+        //float value1 = toDisableInput.action.ReadValue<float>();
+        //Debug.Log(value1);
+        // disable disired input
+        // this time: left hand activate value 
+        for (int i=0; i<toDisableInputs.Length;i++)
+        {
+            toDisableInputs[i].action.Disable();
+        }
+    }
+
+    private void ForceMaintainGrab(SelectExitEventArgs args)
+    {
+         leftHandInteractor.interactionManager.SelectEnter(leftHandInteractor, objectToGrab);
+    }
+}
--- Assets/Scripts/HandControl/RightHandControl.cs
+++ Assets/Scripts/HandControl/RightHandControl.cs
@@ -3,17 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class AnimateHandOnInput : MonoBehaviour
+public class RightHandControl : MonoBehaviour
 {
     public InputActionProperty pinchAnimationAction;
     public InputActionProperty gripAnimationAction;
     public Animator handAnimator;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-            
-    }
 
     // Update is called once per frame
     void Update()
@@ -23,6 +17,5 @@ public class AnimateHandOnInput : MonoBehaviour
 
         float gripValue = gripAnimationAction.action.ReadValue<float>();
         handAnimator.SetFloat("Grip", gripValue);
-
     }
 }
