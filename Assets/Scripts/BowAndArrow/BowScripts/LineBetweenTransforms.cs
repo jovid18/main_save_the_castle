f2e7afaa044a13575df4d76e3b9d1f6b6814ba01
Dongho <pridesam@snu.ac.kr>
2023-12-08 16:58:20 +0900

    Resolve left and right hand controller collider conflicting
    So that we now get perfect, non-conflicted bow.
    How I do that?
    1. disable left hand controller collider. It's because that when right hand try to trigger grab point of bow, both colliders get conflict.

--- Assets/Scripts/BowAndArrow/BowScripts/LineBetweenTransforms.cs
+++ Assets/Scripts/BowAndArrow/BowScripts/LineBetweenTransforms.cs
@@ -1,3 +1,40 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+///// <summary>
+///// Given an array of transforms, draw a line With unitys line renderer between those
+///// </summary>
+//[RequireComponent(typeof(LineRenderer))]
+
+////uncomment next line to have editor draw the line between the transforms outside playmode.
+////You can then recomment it and the line will stay drawn.
+////otherwise Editor is always redrawing it for no reason.
+//[ExecuteInEditMode]
+
+//public class LineBetweenTransforms : MonoBehaviour
+//{
+//    public Transform[] transforms;
+//    public float lineWidth = .01f;
+//    LineRenderer _lineRenderer;
+
+//    void Start()
+//    {
+//        // get attached component T(LineRenderer) of current object
+//        _lineRenderer = GetComponent<LineRenderer>();
+//        _lineRenderer.positionCount = transforms.Length; // 3
+//        _lineRenderer.widthMultiplier = lineWidth;
+//    }
+
+//    void Update()
+//    {
+//        for(int i =0; i < transforms.Length; i++)
+//        {
+//            _lineRenderer.SetPosition(i, transforms[i].position);
+//        }
+//    }
+//}
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,15 +57,14 @@ public class LineBetweenTransforms : MonoBehaviour
 
     void Start()
     {
-        // get attached component T(LineRenderer) of current object
         _lineRenderer = GetComponent<LineRenderer>();
-        _lineRenderer.positionCount = transforms.Length; // 3
+        _lineRenderer.positionCount = transforms.Length;
         _lineRenderer.widthMultiplier = lineWidth;
     }
 
     void Update()
     {
-        for(int i =0; i < transforms.Length; i++)
+        for (int i = 0; i < transforms.Length; i++)
         {
             _lineRenderer.SetPosition(i, transforms[i].position);
         }
